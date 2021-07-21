5afeed5d7083cc4e43269bccd3461dba59ee072d
Lakiprog <szabadib.laci999@gmail.com>
2021-07-21 13:48:31 +0200

    napravljen handler svih korisnika

---
+++ SparkREST/src/handlers/KorisnikHandler.java
@@ -0,0 +1,173 @@
+package handlers;
+
+import java.util.ArrayList;
+
+import domain.Admin;
+import domain.Korisnik;
+import domain.Kupac;
+import domain.Prodavac;
+
+public class KorisnikHandler {
+
+	private KupciHandler kupciHandler;
+	private AdminHandler adminHandler;
+	private ProdavacHandler prodavacHandler;
+	private ArrayList<Korisnik> korisnici;
+	
+	public KorisnikHandler() {
+		kupciHandler = new KupciHandler();
+		adminHandler = new AdminHandler();
+		prodavacHandler = new ProdavacHandler();
+		
+		for (Kupac kupac : kupciHandler.getkupci()) {
+			korisnici.add(kupac);
+		}
+		
+		for (Admin admin : adminHandler.getAdmini()) {
+			korisnici.add(admin);
+		}
+		
+		for(Prodavac prodavac : prodavacHandler.getProdavci()) {
+			korisnici.add(prodavac);
+		}
+	}
+	
+	public boolean postojiKorisnickoIme(String kIme) {
+		for (Korisnik korisnik : korisnici) {
+			if(korisnik.getkIme().equals(kIme)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public ArrayList<Korisnik> getKorisnici() {
+		return korisnici;
+	}
+	
+	public ArrayList<Kupac> getKupci() {
+		return kupciHandler.getkupci();
+	}
+	
+	public ArrayList<Admin> getAdmini() {
+		return adminHandler.getAdmini();
+	}
+	
+	public ArrayList<Prodavac> getProdavci() {
+		return prodavacHandler.getProdavci();
+	}
+	
+	public void addKupac(Kupac k) {
+		korisnici.add(k);
+		kupciHandler.dodajKupca(k);
+	}
+	
+	public void addAdmin(Admin a) {
+		korisnici.add(a);
+		adminHandler.dodajAdmina(a);
+	}
+	
+	public void addProdavac(Prodavac p) {
+		korisnici.add(p);
+		prodavacHandler.dodajProdavca(p);
+	}
+	
+	public void updateKupac(Kupac k) {
+		for (Korisnik korisnik : korisnici) {
+			if((korisnik instanceof Kupac) && (korisnik.getId() == k.getId())) {
+				Kupac kupac = (Kupac) korisnik;
+				kupac = k;
+				break;
+			}
+		}
+		kupciHandler.azurirajKupca(k);
+	}
+	
+	public void updateAdmin(Admin a) {
+		for (Korisnik korisnik : korisnici) {
+			if((korisnik instanceof Admin) && (korisnik.getId() == a.getId())) {
+				Admin admin = (Admin) korisnik;
+				admin = a;
+				break;
+			}
+		}
+		adminHandler.azurirajAdmina(a);
+	}
+	
+	public void updateProdavac(Prodavac p) {
+		for (Korisnik korisnik : korisnici) {
+			if((korisnik instanceof Prodavac) && (korisnik.getId() == p.getId())) {
+				Prodavac prodavac = (Prodavac) korisnik;
+				prodavac = p;
+				break;
+			}
+		}
+		prodavacHandler.azurirajProdavca(p);
+	}
+	
+	public void deleteKupac(Kupac k) {
+		for (int i = 0; i < korisnici.size(); i++) {
+			if((korisnici.get(i) instanceof Kupac) && (korisnici.get(i).getId() == k.getId())) {
+				korisnici.get(i).setObrisan(true);
+				break;
+			}
+		}
+		kupciHandler.brisiKupcaLogicki(k.getId());
+	}
+	
+	public void deleteAdmin(Admin k) {
+		for (int i = 0; i < korisnici.size(); i++) {
+			if((korisnici.get(i) instanceof Admin) && (korisnici.get(i).getId() == k.getId())) {
+				korisnici.get(i).setObrisan(true);
+				break;
+			}
+		}
+		adminHandler.brisiAdminaLogicki(k.getId());
+	}
+	
+	public void deleteProdavac(Prodavac k) {
+		for (int i = 0; i < korisnici.size(); i++) {
+			if((korisnici.get(i) instanceof Prodavac) && (korisnici.get(i).getId() == k.getId())) {
+				korisnici.get(i).setObrisan(true);
+				break;
+			}
+		}
+		prodavacHandler.brisiProdavcaLogicki(k.getId());
+	}
+	
+	public int nextIdKupac() {
+		return kupciHandler.nextId();
+	}
+	
+	public int nextIdProdavac() {
+		return prodavacHandler.nextId();
+	}
+	
+	public int nextIdAdmin() {
+		return adminHandler.nextId();
+	}
+	
+	public Kupac poIdKupac(int id) {
+		return kupciHandler.poId(id);
+	}
+	
+	public Admin poIdAdmin(int id) {
+		return adminHandler.poId(id);
+	}
+	
+	public Prodavac poIdProdavac(int id) {
+		return prodavacHandler.poId(id);
+	}
+	
+	public Kupac poKorisnickomImenuKupac(String kIme) {
+		return kupciHandler.poKorisnickomImenu(kIme);
+	}
+	
+	public Admin poKorisnickomImenuAdmin(String kIme) {
+		return adminHandler.poKorisnickomImenu(kIme);
+	}
+	
+	public Prodavac poKorisnickomImenuProdavac(String kIme) {
+		return prodavacHandler.poKorisnickomImenu(kIme);
+	}
+}
